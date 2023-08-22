313d4af2055a2ab7691792cd5267dda90bad090d
Matti Luukkainen <mluukkai@iki.fi>
2023-08-22 11:59:08 +0300

    part6-1

---
+++ src/components/Notes.jsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux' // highlight-line
+import { toggleImportanceOf } from '../reducers/noteReducer' // highlight-line
+
+const Note = ({ note, handleClick }) => {
+  return(
+    <li onClick={handleClick}>
+      {note.content} 
+      <strong> {note.important ? 'important' : ''}</strong>
+    </li>
+  )
+}
+
+const Notes = () => {
+  const dispatch = useDispatch() // highlight-line
+  const notes = useSelector(state => state) // highlight-line
+
+  return(
+    <ul>
+      {notes.map(note =>
+        <Note
+          key={note.id}
+          note={note}
+          handleClick={() => 
+            dispatch(toggleImportanceOf(note.id))
+          }
+        />
+      )}
+    </ul>
+  )
+}
+
+export default Notes
