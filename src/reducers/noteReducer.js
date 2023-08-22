ad87fcad9367c119e18f3c1d7810b934ba35caa4
Matti Luukkainen <mluukkai@iki.fi>
2023-08-22 13:18:27 +0300

    tweak

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,21 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
